e66cd6ee472c4be0f69c1a6a55ada1a9c5b2ce44
uavteambyu <uavteambyu@gmail.com>
2017-12-08 12:44:21 -0700

    stuff

--- load_params_cal.bash
+++ load_params_cal.bash
@@ -1,6 +1,7 @@
 set -x
 
-rosservice call /fixedwing/param_load_from_file ~/rosflight_ws/param.yml
+rosservice call /fixedwing/param_load_from_file ~/strato.yml
 rosservice call /fixedwing/calibrate_imu
 rosservice call /fixedwing/calibrate_baro
 rosservice call /fixedwing/calibrate_airspeed
+rosservice call /fixedwing/param_write
