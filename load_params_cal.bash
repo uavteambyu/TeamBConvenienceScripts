122d749ff2236f2854d72fc19e8a3326142a33e2
uavteambyu <uavteambyu@gmail.com>
2017-12-12 13:57:54 -0700

    bomb_Drop

--- load_params_cal.bash
+++ load_params_cal.bash
@@ -1,6 +1,6 @@
 set -x
 
-rosservice call /fixedwing/param_load_from_file ~/rosflight_ws/src/TeamBConvenience/strato.yml
+rosservice call /fixedwing/param_load_from_file ~/rosflight_ws/src/TeamBConvenienceScripts/strato.yml
 rosservice call /fixedwing/calibrate_imu
 rosservice call /fixedwing/calibrate_baro
 rosservice call /fixedwing/calibrate_airspeed
